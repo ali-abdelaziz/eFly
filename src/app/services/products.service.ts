59c13113c35afdff765b4f57be8c4e2db1a008e1
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2024-10-28 16:19:39 +0300

    cach data in local storage to avoid multiple requests

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,7 +2,7 @@ import { Injectable, signal, WritableSignal } from '@angular/core';
 import { environment } from '../../environment/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Product } from '../models/product.model';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,20 +17,58 @@ export class ProductsService {
   constructor(private http: HttpClient) { }
 
   getAllProducts(page = 1, limit = 5, search = '') {
-    let params = new HttpParams();
-    if (page) params = params.append('page', page);
-    if (limit) params = params.append('limit', limit);
-    if (search) params = params.append('search', search);
-
-    return this.http.get<Product[]>(this.api, { params })
+    // caching data in local storage to avoid multiple requests
+    const cachedProducts = localStorage.getItem('products');
+    if (!cachedProducts) {
+      console.log('No cached products found. Making a request...');
+      let params = new HttpParams();
+      if (page) params = params.append('page', page);
+      if (limit) params = params.append('limit', limit);
+      if (search) params = params.append('search', search);
+      return this.http.get<Product[]>(this.api, { params })
+      .pipe(
+        tap((products) => {
+          localStorage.setItem('products', JSON.stringify(products));
+        })
+      )
+    } else {
+      console.log('Using cached products...');
+      return new BehaviorSubject<Product[]>(JSON.parse(cachedProducts));
+    }
   }
-
   getCategories() {
-    return this.http.get<string[]>(this.api + '/categories');
+    // caching data in local storage to avoid multiple requests
+    const cachedCategories = localStorage.getItem('categories');
+    if (!cachedCategories) {
+      console.log('No cached categories found. Making a request...');
+      return this.http.get<string[]>(this.api + '/categories')
+      .pipe(
+        tap((categories) => {
+          localStorage.setItem('categories', JSON.stringify(categories));
+        })
+      )
+    } else {
+      console.log('Using cached categories...');
+      return new BehaviorSubject<string[]>(JSON.parse(cachedCategories));
+    }
   }
 
   getProductsByCategory(category: string) {
-    return this.http.get<Product[]>(this.api + '/category/' + category);
+    // caching data in local storage to avoid multiple requests
+    const cachedProductsByCategory = localStorage.getItem(category);
+    if (!cachedProductsByCategory) {
+      console.log('No cached products by category found. Making a request...');
+      return this.http.get<Product[]>(this.api + '/category/' + category)
+      .pipe(
+        tap((products) => {
+          localStorage.setItem(category, JSON.stringify(products));
+        })
+      )
+    } else {
+      console.log('Using cached products by category...');
+      return new BehaviorSubject<Product[]>(JSON.parse(cachedProductsByCategory));
+    }
+    // return this.http.get<Product[]>(this.api + '/category/' + category);
   }
 
   AddProduct(product: Product) {
